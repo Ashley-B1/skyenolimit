06260581bca1b4726ead8025b49d3608c16659d3
Ashley (Skye) Brown <stormi.seuneu@outlook.com>
2022-07-25 09:46:43 -0500

    adjust about me to be

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,29 +1,51 @@
-import React from 'react';
+import React from "react";
 
-import './About.css'
-import aboutPhoto from '../../images/dev-photos/main-photo.jpeg'
+import "./About.css";
+import aboutPhoto from "../../images/dev-photos/main-photo.jpeg";
 
 const About = () => {
   return (
-    <div className='about'>
-      <div className='about-left'>
-        <div className='about-card bg'></div>
-        <div className='about-card'>
-          <img src={aboutPhoto} alt='dev' className='about-image' />
+    <div className="about">
+      <div className="about-left">
+        <div className="about-card bg"></div>
+        <div className="about-card">
+          <img src={aboutPhoto} alt="dev" className="about-image" />
         </div>
       </div>
-      <div className='about-right'>
-        <h1 className='about-title'>About Me</h1>
-        <p className='about-sub'>In summer 2020, I started my coding journey and never looked back. I practiced building static websites on my local machine with HTML and CSS. I would build out wireframes using paper and dry erase boards.</p>
-        <p className='about-desc'>
-          I started App Academy April of 2021 and did well in my classes, asked great questions, and was a great peer programmer, however when classes were over I had to face my personal demons. I got sick 2 times during my cohort and had to be deferred. I was also in a very toxic living situation, so I was not able to focus on code outside of school. I felt exhausted both in and out of class, and had very little support. I felt defeated and lost and did not know what to do. I loved coding, I felt so much satisfaction debugging and building out websites.
+      <div className="about-right">
+        <h1 className="about-title">About Me</h1>
+        <p className="about-sub">
+          My name is Skye and I'm glad you found my portfolio! I've been working
+          with computers all my life. I was actually introduced to my first
+          computer program in 1999. I've been coding for about 5 years and fell
+          in love with frontend development.
         </p>
-        <p className='about-desc ab2'>
-          After my last deferral in February of 2022, I realized I have all the skills for front-end development such as HTML, CSS, Javascript, React, React-Redux, and Pug. I also have some backend skills such as Express, Sequelize, and PostgreSQL. Bootcamp did help me get and use the proper resources to be a developer such as GitHub and explaining what and why we use certain syntax over another. I also have several peers that were able to graduate that are now my mentors. I'm glad boot camps surrounded me with like-minded peers and gave me an in-depth scope of what software development is.
+        <p className="about-desc">
+          I am a frontend developer who took her love of technology and art and
+          turned it into a career. I'm here to break the barriers down for
+          women, especially WOC in the STEM field. The first time I ever touched
+          a computer was in 1999! I was 2 years old when my granny, a former
+          Apple employee, introduced me to the World Wide Web. We used to play
+          games on her computer as a form of bonding. It's is quite odd to say
+          my granny was my gateway to technology but it's true. My granny was a
+          tech savvy woman who passed her love for modern technology down to me.
+          Now that I have the torch, I want ignite the path for other women to
+          take charge in the tech field. I have all the skills for front-end
+          development such as HTML, CSS, Javascript, React, React-Redux, and
+          Pug. I also have some backend skills such as Express, Sequelize, and
+          PostgreSQL. My favorite programs are React and SCSS. I'm currently
+          based in Dallas, TX but hope to relocate soon.
+        </p>
+        <p className="about-desc ab2">
+          I build out wireframes using paper, dry erase boards, and Figma. I use
+          resources like geeks for geeks, W3 Schools, MDN, and stack overflow
+          for troubleshooting. I am immersed in all my projects and want to dive
+          deeper into development to see how to make fully functional and
+          user-friendly websites.
         </p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
