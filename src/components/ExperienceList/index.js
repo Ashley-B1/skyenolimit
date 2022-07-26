62f8197d0334c471766c875281822d9d1cdc6b26
Ashley (Skye) Brown <stormi.seuneu@outlook.com>
2022-07-26 10:37:20 -0500

    added final style

--- src/components/ExperienceList/index.js
+++ src/components/ExperienceList/index.js
@@ -1,18 +1,27 @@
-import React from 'react';
-import './ExperienceList.css';
-import { experience } from '../../data';
-import Experience from '../Experience';
+import React from "react";
+import "./ExperienceList.css";
+import { experience } from "../../data";
+import Experience from "../Experience";
 
 const ExperienceList = () => {
   return (
-    <div className='work-exp'>
-      {experience.map(exp => (
-        <div className='exp-container'>
-          <Experience key={exp.id} title={exp.title} company={exp.company} date={exp.date} skills={exp.skills} />
-        </div>
-      ))}
+    <div className="work-exp">
+      <h2 className="my-experience">Work Experience</h2>
+      <section className="exp-list">
+        {experience.map((exp) => (
+          <div className="exp-container">
+            <Experience
+              key={exp.id}
+              title={exp.title}
+              company={exp.company}
+              date={exp.date}
+              skills={exp.skills}
+            />
+          </div>
+        ))}
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default ExperienceList
+export default ExperienceList;
